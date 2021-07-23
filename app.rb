ca14bddb209433fdfe81ae7be9b1330aadb8861e
Gengur123 <meleearthur@gmail.com>
2021-07-23 13:26:30 -0700

    Added a homepage to the website

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
-require 'sinatra'
-require 'sinatra/reloader'
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
 also_reload 'lib/**/*.rb'
 require 'pry'
 
-get '/' do
-  "Hellow Word"
+get ('/') do
+  @words = Word.all
+  erb(:words)
 end
