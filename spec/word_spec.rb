13ec7783ea64fc04ce1bc344e00445e713a8f701
Gengur123 <meleearthur@gmail.com>
2021-07-23 10:28:35 -0700

    Fourth Test pass

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -9,27 +9,37 @@ describe '#Word' do
   end
 
   describe('#Word') do
-    it('creates a new word and stores it') do
+    it('should create a new word and stores it') do
       word1 = Word.new('swag', nil)
       expect(word1.inputword).to(eq('swag'))
     end
   end
 
   describe('.all') do
-    it('returns and empty array where inputed words would be stored') do
+    it('should return an empty array where inputed words would be stored') do
       expect(Word.all).to(eq([]))
     end
   end
   
   describe('#save') do
-    it('stores a word object in a hash, mocking a database') do
+    it('should store the new words in a hash') do
       word1 = Word.new('swag', nil)
       word1.save()
       word2 = Word.new('bruh', nil)
       word2.save()
       word3 = Word.new('kekw', nil)
       word3.save()
-      expect(Word.all).to(eq([word1, word2, word3]))
+      word4 = Word.new('omegalul', nil)
+      word4.save()
+      expect(Word.all).to(eq([word1, word2, word3, word4]))
+    end
+  end
+
+  describe('#==') do
+    it('') do
+      word1 = Word.new('swag', nil)
+      word2 = Word.new('swag', nil)
+      expect(word1).to(eq(word2))
     end
   end
 
