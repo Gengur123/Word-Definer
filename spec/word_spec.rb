e730bbf977d599078bffd3ea44c3cc5b383ac288
Gengur123 <meleearthur@gmail.com>
2021-07-23 10:04:35 -0700

    Second and third test passing

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -9,9 +9,28 @@ describe '#Word' do
   end
 
   describe('#Word') do
-    it('creates a new word object with an entered name and a unique id') do
-      word1 = Word.new('bubblefy', nil)
-      expect(word1.inputword).to(eq('bubblefy'))
+    it('creates a new word and stores it') do
+      word1 = Word.new('swag', nil)
+      expect(word1.inputword).to(eq('swag'))
     end
   end
+
+  describe('.all') do
+    it('returns and empty array where inputed words would be stored') do
+      expect(Word.all).to(eq([]))
+    end
+  end
+  
+  describe('#save') do
+    it('stores a word object in a hash, mocking a database') do
+      word1 = Word.new('swag', nil)
+      word1.save()
+      word2 = Word.new('bruh', nil)
+      word2.save()
+      word3 = Word.new('kekw', nil)
+      word3.save()
+      expect(Word.all).to(eq([word1, word2, word3]))
+    end
+  end
+
 end
